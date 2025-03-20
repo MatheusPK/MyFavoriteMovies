442a26a5a9df6e84bbaa9bf9c0687809ab669902
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2025-03-20 07:08:47 -0300

    [ADD] Xcode project

---
+++ MyFavoriteMovies/MyFavoriteMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Matheus Pereira Kulick on 20/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
