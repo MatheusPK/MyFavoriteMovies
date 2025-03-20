9c1609bf7b2f569095ce466eebff9a605f391d41
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2025-03-20 20:52:09 -0300

    [ADD] SearchViewController

--- MyFavoriteMovies/MyFavoriteMovies/Scenes/Search/SearchViewController.swift
+++ MyFavoriteMovies/MyFavoriteMovies/Scenes/Search/SearchViewController.swift
@@ -9,9 +9,96 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    private lazy var searchStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [searchBar, searchButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 2
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Find your favorite movie..."
+        searchBar.searchTextField.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        searchBar.searchBarStyle = .minimal
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
+    private lazy var searchButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.setTitle("Search", for: .normal)
+        button.addTarget(self, action: #selector(searchButtonWasTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var loadingView: UIActivityIndicatorView = {
+        let progressView = UIActivityIndicatorView(style: .large)
+        progressView.hidesWhenStopped = true
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        return progressView
+    }()
+    
+    private lazy var errorView: ErrorView = {
+        let errorView = ErrorView()
+        errorView.translatesAutoresizingMaskIntoConstraints = false
+        errorView.isHidden = true
+        return errorView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        title = "Search"
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func searchButtonWasTapped() {
+        
+    }
+}
+
+extension SearchViewController: ViewCode {
+    func addSubviews() {
+        view.addSubview(searchStackView)
+        view.addSubview(loadingView)
+        view.addSubview(errorView)
+    }
+    
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            searchStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            
+            loadingView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loadingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            errorView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            errorView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            errorView.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 8),
+            errorView.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -8)
+        ])
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchButtonWasTapped()
+    }
 }
